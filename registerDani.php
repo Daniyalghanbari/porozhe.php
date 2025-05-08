4ce54f64ca44721b3ba1901171d506548e2cb912
Daniyalghanbari <daniyalghanbari706@gmail.com>
2025-05-08 11:27:26 +0100

    Update registerDani.php

--- registerDani.php
+++ registerDani.php
@@ -5,11 +5,10 @@
 /*
 | request 
 */
-$requestUsername = $_REQUEST['username'];
-$requestPassword = $_REQUEST['password'];
+$requestUsername = $_REQUEST['username'] ?? null;
+$requestPassword = $_REQUEST['password'] ?? null;
 
-include "./register.html";
-
+include "./view/register.html";
 
 /*
 | logic
@@ -62,5 +61,4 @@ function select($query){
 
    return $data->fetchAll();
 }
-
 
