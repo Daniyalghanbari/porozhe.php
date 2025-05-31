c1f391d8eabcd0bfaecfec6e0e58f817d354835b
Daniyalghanbari <daniyalghanbari706@gmail.com>
2025-05-31 20:35:12 +0330

    Update loginDani.php

--- loginDani.php
+++ loginDani.php
@@ -1,38 +1,43 @@
  <?php
+session_start();
 
 include "./helper.php";
-include "./view/login.html";
-session_start();
 $connection = connetion();
 
-/*
+/*-----------------------
 | request validation
-*/
+ ------------------------*/
 $requestUsername = $_REQUEST['username'] ?? null;
 $requestPassword = $_REQUEST['password'] ?? null;
 
-if(empty($requestUsername) 
-|| empty($requestPassword)
-){
 
-    echo "empty username or password";
-    return;
+if($_SERVER['REQUEST_METHOD']== "POST"){
+
+    if(empty($requestUsername) 
+    || empty($requestPassword)
+    ){
+        $_SESSION['error'] = "نام کاربری و کلمه عبور نمیتواند خالی باشد";
+    }
+
+    if(!empty($requestUsername)){
+        /*--------------------
+        | validation 
+          --------------------*/
+        $userData = select($connection,"SELECT * FROM `users` WHERE `username` = '$requestUsername'; ")[0] ?? null;
+        if(empty($userData)){
+
+            $_SESSION['error'] = "کاربری با این شناسه وجود ندارد";
+        }
+
+        /*-------------------------------------------------------------
+        | login and set user id session and redirect to adminarea
+          ------------------------------------------------------------*/
+        if(!empty($userData)){
+            $_SESSION['user_id'] = $userData['id'];
+
+            header("Location: admin.php");
+        }
+    }
 }
 
-/*
-| validation 
-|
-*/
-$userData = select($connection,"SELECT * FROM `users` WHERE `username` = '$requestUsername'; ")[0] ?? null;
-if(empty($userData)){
-
-    echo "username is not empty";
-    return;
-}
-
-/*
-| logic
-*/
-$_SESSION['user_id'] = $userData['id'];
-
-header("Location: admin.php");
+include "./view/login.php";
