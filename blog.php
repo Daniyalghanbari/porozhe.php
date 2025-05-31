b3833de91ab345209ff4effaa210580caac1bfa4
Daniyalghanbari <daniyalghanbari706@gmail.com>
2025-05-31 20:29:41 +0330

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,30 +1,35 @@
-<?php
+ <?php
 session_start();
 include "./helper.php";
-include_once "./view/blog/create.html";
 
 $connection = connetion();
 
-$_SEESSION['error'] = "test222";
-
-
 /*
-| request 
+| request initial
 */
 $requestTitle  = $_REQUEST['title'] ?? null;
 $requestDescription = $_REQUEST['description'] ?? null;
 
+/* --------------------
+| validation request
+ ----------------------*/
 if(empty($requestTitle) 
 || empty($requestDescription)
 ){
 
-    echo "empty username or password";
-    return;
+    $_SESSION['error'] = "مقادیر را لطفا پر نمایید";
+}else {
+
+    /* -----------------
+    | create blog
+     ------------------*/
+    execute($connection,
+    "INSERT INTO `blogs` (`id`, `title`, `description`) 
+                VALUES (NULL, '$requestTitle', '$requestDescription');" 
+    );
+
+    $_SESSION['succes'] = "با موفقیت ایجاد شد";
+
 }
 
-execute($connection,
-"INSERT INTO `blogs` (`id`, `title`, `description`) 
-              VALUES (NULL, '$requestTitle', '$requestDescription');" 
-);
-
-header("Location: index.php");
+header("Location: admin.php");
